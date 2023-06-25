6a978d7eae997ed0a65589648072998085fa7373
Woopy <eduard.valceanu@yahoo.com>
2023-06-25 21:16:34 +0300

    Working

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-function currentRoom(){
-  const button = document.getElementById("room");
-  const buttonPressed = (e) => {
-    e.target.classList.toggle("current");
+const BUTTONS = document.getElementsByTagName('button');
+function getButtonId() {
+  let currentButton = null;
+  for (let i = 1; i < (BUTTONS.length+1); i++) {
+    BUTTONS[i].addEventListener('click', function() {
+      if (currentButton !== null) {
+        currentButton.classList.remove('current');
+        currentButton.classList.add('visited-room');
+        currentButton.textContent = '';
+      }   
+      this.classList.add('current');
+      this.textContent = 'PLAYER';
+      currentButton = this;
+      document.getElementById('RESET').addEventListener('click', function(){
+        currentButton = null;
+      })
+    });
   }
-  button.addEventListener("click", buttonPressed);
 }
+
+function reset() {
+  const resetButton = document.getElementById('RESET');
+  resetButton.addEventListener('click', function(){
+    let currentButton = null;
+  for (let i = 1; i < (BUTTONS.length+1); i++) {
+    BUTTONS[i].classList.remove('current');
+    BUTTONS[i].classList.remove('visited-room');
+    BUTTONS[i].textContent = '';
+  }
+  })
+}
+
+
+
+
